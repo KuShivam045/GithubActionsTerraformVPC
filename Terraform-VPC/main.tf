f588cfcdc41c06b92918baca44f45838c977e3ff
Shivam Kumar <kumarshivam9318@gmail.com>
2024-01-18 18:27:13 +0530

    ALB

--- Terraform-VPC/main.tf
+++ Terraform-VPC/main.tf
@@ -8,3 +8,16 @@ module "sg"{
     source = "./modules/sg"
     vpc_id = module.vpc.vpc_id
 }
+ module "ec2" {
+   source = "./modules/ec2"
+   sg_id = module.sg.sg_id
+   subnets = module.vpc.subnet_ids
+}
+
+module "alb" {
+    source = "./modules/alb"
+    sg_id = module.sg.sg_id
+    subnets = module.vpc.subnet_ids
+    vpc_id = module.vpc.vpc_id
+    instances = module.ec2.instances
+}
